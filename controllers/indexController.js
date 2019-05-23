8f02293f998dfb5b7d3386483b5d140c237fab6a
Daniel Tineli <danieltineli.td@gmail.com>
2019-05-23 11:17:33 -0300

    Final

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,7 +4,6 @@ exports.index = (req, res, next) => {
     const chamados = [{ nome: 'Erro', motivo: 'Erro', empresa: 'Erro' }];
     axios.get('http://127.0.0.1:3000/chamado')
         .then((response) => {
-            console.log(response.status);
             res.render('index', {
                 chamados: response.data.chamados
             });
@@ -14,20 +13,4 @@ exports.index = (req, res, next) => {
                 chamados: chamados
             })
         });
-}
-
-exports.postIndex = (req, res, next) => {
-    if (req.params.id) {
-        axios.post('http://127.0.0.1:3000/chamado/' + req.params.id, {
-            finalizado: 'S',
-            data_finalizado: new Date
-        })
-            .then((response) => {
-                res.redirect('/');
-            }).catch((err) => {
-                console.log(err);
-                res.redirect('/');
-            });
-    }
-
 }
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,7 +4,6 @@ const indexController = require('../controllers/indexController');
 
 const router = express.Router();
 
-router.get('/:id', indexController.postIndex);
 router.get('/', indexController.index);
 
 module.exports = router;
